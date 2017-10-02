0afa56455c44ede56e6e9cf5fbd458fa6cef9f7a
franck <franck.moothery@ensea.fr>
2017-10-02 17:46:00 +0200

    test

--- src/shared/state.h
+++ src/shared/state.h
@@ -2,5 +2,21 @@
 #ifndef __STATE__H
 #define __STATE__H
 
-#include "state/Exemple.h"
+#include "state/ElementTab.h"
+#include "state/Element.h"
+#include "state/StaticElement.h"
+#include "state/ChampdebatailleTypeId.h"
+#include "state/Champdebataille.h"
+#include "state/BatimentTypeId.h"
+#include "state/Batiment.h"
+#include "state/MobileElement.h"
+#include "state/InfanterieTypeId.h"
+#include "state/Infanterie.h"
+#include "state/HeliTypeId.h"
+#include "state/Heli.h"
+#include "state/TypeId.h"
+#include "state/TankTypeId.h"
+#include "state/Tank.h"
+#include "state/ElementChars.h"
+#include "state/State.h"
 #endif
