a4042a4a5108d69e513665344d227ce0f70446ef
boutaleb <yousr.boutaleb@gmail.com>
2017-10-03 14:09:17 +0200

    supp

---
+++ src/shared/state/heli.cpp
@@ -0,0 +1,26 @@
+#include "Heli.h"
+
+
+
+namespace state{
+    Heli::Heli() {
+        
+    }
+
+     const TypeId Heli::getTypeId() {
+
+    }
+    Heli::~Heli() {
+
+    }
+ 
+     int Heli::diffUnite() {
+
+    }
+     
+
+
+ 
+
+
+}
