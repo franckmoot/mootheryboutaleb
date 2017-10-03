1a20fe8ae113e66b82d4aac7289f02a6c9abe2c4
franck <franck.moothery@ensea.fr>
2017-10-03 09:51:13 +0200

    test

---
+++ src/shared/state/Element.cpp
@@ -0,0 +1,19 @@
+#include "Element.h"
+
+
+namespace state {
+
+    Element::Element() {}
+       
+    const TypeId Element::getTypeId() {
+        
+        return TypeId;
+    }
+
+
+    Element::~Element() {
+
+    }
+
+    
+}
