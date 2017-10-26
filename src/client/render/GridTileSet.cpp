34c745d55b9992491769555a40206d6c6b71d5a7
franck <franck.moothery@ensea.fr>
2017-10-26 18:19:30 +0200

    dfsdfds

--- src/client/render/GridTileSet.cpp
+++ src/client/render/GridTileSet.cpp
@@ -7,42 +7,84 @@ using namespace std;
 using namespace state;
 
 namespace render {
+  
+  GridTileSet::GridTileSet() {
+    
+    chpbataille.push_back(*(new Tile(0,0,16,16)));
+    chpbataille.push_back(*(new Tile(16,0,16,16)));
+    chpbataille.push_back(*(new Tile(32,0,16,16)));
+    chpbataille.push_back(*(new Tile(48,0,16,16)));
+    chpbataille.push_back(*(new Tile(64,0,16,16)));
+    batiment.push_back(*(new Tile(0,16,16,30)));
+    batiment.push_back(*(new Tile(16,32,16,16))); 
+  }
+  
+  
+  const std::string GridTileSet::getImageFile() const {
+    return "res/tilemap.png";
+  }
+  
+  
+  const Tile& GridTileSet::getTile(const state::Element& e) const {
 
-    GridTileSet::GridTileSet() {
+    MobileElement* E = (MobileElement*)&e;
+    if(E->isStatic()){
+      Champdebataille* C=(Champdebataille*)&E;
+      if(C->isChampdebataille()){
+	
         
-        chpbataille.push_back(*(new Tile(0,0,16,16)));
-        chpbataille.push_back(*(new Tile(16,0,16,16)));
-        chpbataille.push_back(*(new Tile(32,0,16,16)));
-        chpbataille.push_back(*(new Tile(48,0,16,16)));
-        chpbataille.push_back(*(new Tile(64,0,16,16)));
-        batiment.push_back(*(new Tile(0,16,16,30)));
-        batiment.push_back(*(new Tile(16,32,16,16))); 
+	switch (C->getChampdeBatailleType()){
+	  
+	case state::ChampdebatailleTypeId::EAU:
+	  return chpbataille[ChampdebatailleTypeId::EAU];
+	  break;
+	  
+	case state::ChampdebatailleTypeId::HERBE:     
+	  return chpbataille[state::ChampdebatailleTypeId::HERBE];
+	  break;
+	  
+	case state::ChampdebatailleTypeId::ROCHER:
+	  return chpbataille[state::ChampdebatailleTypeId::ROCHER];
+	  break;
+          
+	case state::ChampdebatailleTypeId::ROUTE:
+	  return chpbataille[state::ChampdebatailleTypeId::ROUTE];
+	  break;
+          
+	case state::ChampdebatailleTypeId::SABLE:
+	  return chpbataille[state::ChampdebatailleTypeId::SABLE];
+	  break;
+          
+	default:
+	  return chpbataille[0];
+	  break;
+	}
+      }
     }
-    
-    const std::string GridTileSet::getImageFile() const {
-        return "res/tilemap.png";
-        
+    else{
+      Batiment* C=(Batiment*)&E;
+      if(!C->isChampdebataille()){
+	
+	switch (C->getBatimentTypeId()){
+	  
+	case state::BatimentTypeId::CASERNE:
+	  return batiment[state::BatimentTypeId::CASERNE];
+	  break;
+	  
+	case state::BatimentTypeId::QG:    
+	  return batiment[state::BatimentTypeId::CASERNE];
+	  break;
+          
+	default:
+	  return batiment[0];
+	  break;
+	}
+	
+      }
     }
-    
-    
-        const Tile& GridTileSet::getTile(const state::Element& e) const {
-     
-        MobileElement* E = (MobileElement*)&e;
-        if(E->isStatic());
-         
-        
-         
-        
-        /*
-        //if((e.isStatic)return chpbataille(state::ChampdebatailleTypeId::HERBE);
-        if((e==state::Champdebataille)&&(e.getType()==state::ChampdebatailleTypeId::ROCHER))return chpbataille(state::ChampdebatailleTypeId::ROCHER);
-        if((e==state::Champdebataille)&&(e.getType()==state::ChampdebatailleTypeId::ROUTE))return chpbataille(state::ChampdebatailleTypeId::ROUTE);
-        if((e==state::Champdebataille)&&(e.getType()==state::ChampdebatailleTypeId::SABLE))return chpbataille(state::ChampdebatailleTypeId::SABLE);
-        if((e==state::Batiment)&&(e.getType()==state::BatimentTypeId::QG))return batiment(state::BatimentTypeId::QG);
-        if((e==state::Batiment)&&(e.getType()==state::BatimentTypeId::CASERNE))return batiment(state::BatimentTypeId::CASERNE);       */ 
-        
-        return *(new Tile(4,4,4,4));
-    }
-        
-    }
+    return chpbataille[0];
+  }
+}
+
+
 
