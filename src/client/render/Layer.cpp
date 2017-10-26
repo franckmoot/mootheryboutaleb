349c21872a57508399ef1d007910e25cdd31c591
boutaleb <yousr.boutaleb@gmail.com>
2017-10-27 00:54:01 +0200

    hjh

---
+++ src/client/render/Layer.cpp
@@ -0,0 +1,33 @@
+#include "Tilemap.h"
+#include "Layer.h"
+#include "TileSet.h"
+#include "state/ElementTab.h"
+#include "state/ElementChars.h"
+
+
+
+
+
+using namespace std;
+using namespace state;
+
+namespace render {
+    
+    void Layer::createcarte(const state::ElementChars& elemCha){
+        for (int i=0;i<elemCha.getHeight()*elemCha.getWidth();i++){
+            std::vector<int> carte;
+            if(elemCha.list[i].) {
+               
+            }
+     
+            }
+            
+            
+        } 
+    }
+    
+    
+    
+    
+}
+
