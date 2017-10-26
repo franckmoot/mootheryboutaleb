5cb3a00c205a3e1bc6a7823d76e063acdaf65c27
franck <franck.moothery@ensea.fr>
2017-10-26 23:24:47 +0200

    ersgrsg

--- src/client/render/Layer.cpp
+++ src/client/render/Layer.cpp
@@ -4,29 +4,7 @@ using namespace std;
 
 namespace render {
         
-    int Layer::Layer() {
-    }
-
-             
-    const Surface * const Layer::getSurface() {
-
-    }
-    
-    const std::shared_ptr<TileSet>& Layer::getTileset() const {
-
-    }
-    void Layer::initSurface() {
-
-    }
-    
-    void Layer::setSurface(surface::Surface*) {
-
-    }
-    
-        
-    void Layer::setTileset(const std::shared_ptr<TileSet>& tileset) {
-
-    }
+   
 
     
 }
--- src/client/render/Layer.h
+++ src/client/render/Layer.h
@@ -5,26 +5,31 @@
 #include <memory>
 
 namespace render {
+  class Tilemap;
   class TileSet;
+};
+namespace state {
+  class ElementTab;
+  class ElementChars;
 }
 
+#include "Tilemap.h"
 #include "TileSet.h"
+#include "state/ElementTab.h"
+#include "state/ElementChars.h"
 
 namespace render {
 
   /// class Layer - 
   class Layer {
+    // Associations
     // Attributes
   protected:
-    std::unique_ptr<Surface> surface;
+    std::unique_ptr<Tilemap> tilemap;
     std::shared_ptr<TileSet> tileset;
-    // Operations
-  public:
-    layer ();
-    const Surface* const getSurface ();
-    void setSurface ( surface::Surface*);
-    void initSurface ();
     // Setters and Getters
+    const std::unique_ptr<Tilemap>& getTilemap() const;
+    void setTilemap(const std::unique_ptr<Tilemap>& tilemap);
     const std::shared_ptr<TileSet>& getTileset() const;
     void setTileset(const std::shared_ptr<TileSet>& tileset);
   };
