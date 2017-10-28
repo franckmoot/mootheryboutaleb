9490460c907d2e029b22ed6d6c18a4a729eb26e9
boutaleb <yousr.boutaleb@gmail.com>
2017-10-28 23:14:14 +0200

    hkkghku

--- src/shared/state/ElementChars.cpp
+++ src/shared/state/ElementChars.cpp
@@ -1,4 +1,15 @@
 #include "ElementChars.h"
+#include "ElementTab.h"
+#include "Champdebataille.h"
+#include "Batiment.h"
+#include "Element.h"
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <sstream>
 
 
 using namespace std;
@@ -10,7 +21,7 @@ namespace state{
  const std::vector<int> ElementChars::ElementToCarte (std::vector<int> carte){
          for(int i=0;i<(int)list.size();i++){
              if(list[i]=NULL) carte[i]=-1;
-             else carte[i]=list[i].get().getTypeId()+5;
+             else carte[i]=list[i].get()->getTypeId()+5;
          }
          return carte;
     
