0dc6d6b2f9e751d9e023f4aba5ea9708434dc488
franck <franck.moothery@ensea.fr>
2017-10-30 00:16:03 +0100

    2.1111

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -42,48 +42,11 @@ int main(int argc,char* argv[]) {
     else if(!s.compare("render")){
       
        testrender();
-      
-      
-      // on définit le niveau à l'aide de numéro de tuiles
-    /*  std::vector<int> level1 =
-	{
-1, 
--1,-1,-1,-1,-1,-1,-1,-1,-1,-1, 
--1,-1,2,-1,-1,-1,0,-1,-1,-1, 
--1,-1,-1,-1,-1,-1,-1,-1,1,-1, 
--1,-1,-1,-1,-1,-1,-1,-1,-1,-1, 
--1,-1,-1,-1,-1,-1,-1,-1,-1,2, 
--1,-1,-1,-1,-1,-1,-1,-1,-1,-1, 
--1,-1,-1,-1,-1,-1,-1,-1,-1,-1, 
--1,-1,-1,-1,0,-1,-1,-1,-1,-1, 
--1,-1,-1,-1,-1,-1,-1,-1,-1,-1, 
--1,-1,-1,-1,-1,-1,-1,-1,-1,-1, 
--1,-1,-1,-1,-1,-1,-1,-1,-1,
-
-	};*/
-      
-      // on crée la tilemap avec le niveau précédemment défini   
-      // on crée la fenêtre
-      
-      //Tilemap map,mapPlayer;
-      /*std::vector<sf::Sprite> Map;*/
-     // std::vector<int> level;
-      
-      
-      
-      //auto tab = map1.lirefichiercsv(level);
-      
-      //if (!map.load("res/tile.png", sf::Vector2u(64, 64),map.lirefichiercsv(level),10 , 10)) return -1;
-      //if (!mapPlayer.load("res/playe.png", sf::Vector2u(64, 64),level1,10 , 10)) return -1;
-      
-      
-      
+ 
       return 0;
     }
     
   }
-  
-  
   
   return 0;
 }
