8070ca723e17fd65dcdecc632d55e2c2b58540db
boutaleb <yousr.boutaleb@gmail.com>
2017-11-07 16:54:33 +0100

    jhku

--- src/shared/engine/MovecharCommand.cpp
+++ src/shared/engine/MovecharCommand.cpp
@@ -1,6 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include "MovecharCommand.h"
+#include "state/State.h"
+#include "state/Element.h"
 
+
+using namespace std;
+namespace engine{
+    
+    
+   MovecharCommand :: MoveCharCommand (state::State& state, int i, int j){
+       //if(state.getGrid()->getElement(j)->getTypeId()
+      // if(state.getChars()->getElement(i)->getTypeId()==;
+       
+       
+       
+   }
+    
+   
+    
+    
+    
+    
+    
+}
