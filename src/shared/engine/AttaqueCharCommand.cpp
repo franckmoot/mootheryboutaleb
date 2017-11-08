b5649c3f5fa56017a2b265e7fc5ec683780997ce
moothery <franck.moothery@ensea.fr>
2017-11-08 23:35:37 +0100

    23h35

--- src/shared/engine/AttaqueCharCommand.cpp
+++ src/shared/engine/AttaqueCharCommand.cpp
@@ -4,8 +4,12 @@
 #include "state/State.h"
 #include "state/Element.h"
 #include "state/MobileElement.h"
+#include "state/Infanterie.h"
+#include "state/Tank.h"
+#include "state/Heli.h"
 
 using namespace std;
+using namespace state;
 namespace engine{
         
     CommandTypeId AttaqueCharCommand::getTypeId() const {
@@ -19,7 +23,10 @@ namespace engine{
         if((state.chars->getElement(i,1)!=NULL)&&(state.chars->getElement(j,1)!=NULL)){
          if((!state.chars->getElement(i,1)->isStatic())&&!state.chars->getElement(j,1)->isStatic()){
           
-                //state::MobileElement* mobileTmpi = (state::MobileElement*)&(state.chars->getElement(i,1));
+            
+	  state::Infanterie* infanterieTmpj = (state::Infanterie*)(state.chars->getElement(j,1));
+	 
+                //= (state::MobileElement*)&(state.chars->getElement(i,1));
                 //state::MobileElement* mobileTmpj = (state::MobileElement*)&(state.chars->getElement(j,1));
                 
          }else{
