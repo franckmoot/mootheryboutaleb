ccedbe514d020443a6b3f402b301f581218cb990
moothery <franck.moothery@ensea.fr>
2017-11-08 22:04:13 +0100

    22h03

--- src/shared/engine/AttaqueCharCommand.cpp
+++ src/shared/engine/AttaqueCharCommand.cpp
@@ -15,12 +15,20 @@ namespace engine{
     
     void AttaqueCharCommand::execute(state::State& state) {
 
-         /*if(!state.chars->getElement(i,1)->isStatic()){
-            if(!state.chars->getElement(j,1)->isStatic()){
-                state::MobileElement* mobileTmpi = (state::MobileElement*)&(state.chars->getElement(i,1));
-                state::MobileElement* mobileTmpj = (state::MobileElement*)&(state.chars->getElement(j,1));
-                //mobileTmpi;
-        */
+        if((state.chars->getElement(i,1)!=NULL)&&(state.chars->getElement(j,1)!=NULL){
+         if((!state.chars->getElement(i,1)->isStatic())&&!state.chars->getElement(j,1)->isStatic()){
+          
+                //state::MobileElement* mobileTmpi = (state::MobileElement*)&(state.chars->getElement(i,1));
+                //state::MobileElement* mobileTmpj = (state::MobileElement*)&(state.chars->getElement(j,1));
+                
+         }else{
+             
+             cout<<"i ou j n'est pas mobileELement"<<endl;
+         }
+        
+    }else{
+             cout<<"i ou j est null"<<endl;
+             
     }
          
     
