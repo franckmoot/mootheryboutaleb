528c07df16252f43024e0703b0ec06ec36b47fbe
franckmoot <https://franckmoot@github.com>
2017-12-13 15:42:30 +0100

    fichier serveur

---
+++ src/server/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <time.h>
+
+#include <vector>
+#include <fstream>
+#include <sstream>
+//include </src/client/touteslesfonctions.h>
+
+#include "state.h"
+#include "render.h"
+#include "engine.h"
+#include "state/Infanterie.h"
+#include "ai/RandomAI.h"
+
+
+using namespace std;
+using namespace state;
+using namespace render;
+using namespace engine;
+using namespace ai;
+
+int main(int argc, char* argv[]) {
+
+    if (argc > 1) {
+        string s;
+        s = argv[1];
+
+        if (!s.compare("hello")) {
+            cout << "Bonjour le monde !" << endl;
+        } else if (!s.compare("record")) {
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+
+        }
+
+    }
+    return 0;
+}
